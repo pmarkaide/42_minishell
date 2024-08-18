6787242e6ecfd77f304ee7f298b8393caedc8224
Pablo Markaide <pablo.markaide@gmail.com>
2024-08-18 12:49:44 +0300

    add ft_split_args function

--- lib/libft/ft_del.c
+++ lib/libft/ft_del.c
@@ -1,36 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split_args.c                                    :+:      :+:    :+:   */
+/*   ft_del.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/16 09:07:27 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/08/18 12:47:22 by pmarkaid         ###   ########.fr       */
+/*   Created: 2024/08/18 12:26:20 by pmarkaid          #+#    #+#             */
+/*   Updated: 2024/08/18 12:44:00 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-t_list	*ft_split_args(char *str, char *delim)
+void	ft_del(void *content)
 {
-	t_list	*tokens;
-	t_list	*token;
-	char	*token_start_pos;
-
-	tokens = NULL;
-	token_start_pos = ft_strtok(str, delim);
-	while (token_start_pos)
+	if (content)
 	{
-		token = ft_lstnew((void *)token_start_pos);
-		if (!token)
-		{
-			ft_lstclear(&tokens, ft_del);
-			return (NULL);
-		}
-		ft_lstadd_back(&tokens, token);
-		token_start_pos = ft_strtok(NULL, delim);
+		free(content);
 	}
-	return (tokens);
 }
