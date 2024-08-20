cd5bcc55d143f9325b3eaf0fd8dc1775436aae60
dbejar-s <149060545+dbejar-s@users.noreply.github.com>
2024-08-21 02:11:55 +0300

    Pipe added to communicate children with parent process regarding exit codes... when there are more than one children, I'm still testing it because it must return the exit code of the last executed command, so the previous children can't overwrite the exit code.

--- src/execution_utils.c
+++ src/execution_utils.c
@@ -3,44 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   execution_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: dbejar-s <dbejar-s@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 20:03:14 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/08/15 12:25:29 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/08/21 02:03:59 by dbejar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	get_exit_code(int status)
-{
-	int	exit_code;
+// extern int	g_exit;
 
-	exit_code = 0;
-	if (WIFSIGNALED(status))
-		exit_code = 128 + WTERMSIG(status);
-	else if (WIFEXITED(status))
-		exit_code = WEXITSTATUS(status);
-	return (exit_code);
-}
+// int	get_exit_code(int status)
+// {
+// 	int	exit_code;
 
-int	wait_processes(pid_t *pid, int cmds)
-{
-	int	i;
-	int	status;
-	int	exit_code;
+// 	exit_code = 0;
+// 	if (WIFSIGNALED(status))
+// 		exit_code = 128 + WTERMSIG(status);
+// 	else if (WIFEXITED(status))
+// 		exit_code = WEXITSTATUS(status);
+// 	return (exit_code);
+// }
 
-	i = 0;
-	exit_code = 0;
-	while (i < cmds)
-	{
-		waitpid(pid[i], &status, 0);
-		if (i == cmds - 1)
-			exit_code = get_exit_code(status);
-		i++;
-	}
-	return (exit_code);
-}
+// int	wait_processes(pid_t *pid, int cmds)
+// {
+// 	int	i;
+// 	int	status;
+// 	int	exit_code;
+
+// 	i = 0;
+// 	exit_code = 0;
+// 	while (i < cmds)
+// 	{
+// 		waitpid(pid[i], &status, 0);
+// 		if (i == cmds - 1)
+// 			exit_code = get_exit_code(status);
+// 		i++;
+// 	}
+// 	return (exit_code);
+// }
 
 char	**build_cmd_args_array(t_token *cmd_args)
 {
