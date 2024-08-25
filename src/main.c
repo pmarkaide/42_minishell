900aa64724cacd56d748461426aa104f769044a6
dbejar-s <149060545+dbejar-s@users.noreply.github.com>
2024-08-25 21:27:56 +0300

    Fix segfault when parsing returns NULL, the code is NULL-protected now

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: dbejar-s <dbejar-s@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 14:49:38 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/08/24 21:06:04 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/08/25 21:25:11 by dbejar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,13 @@ int	main(int argc, char **argv, char **envp)
 			continue;
 		//print_tokens(macro->tokens);
 		macro->cmds = parsing(macro);
+		if (macro->cmds == NULL)
+		{
+			free(line);
+			continue ;
+		}
 		execution(macro);
+		//VALORAR METER AQUI UN CLEAN_MACRO ANTES DE QUE VUELVA EL LOOP: se puede limpiar todo menos el env. No lo quiero meter ahora por no hacer doubles frees, pero probablemente haya que meterlo por temas de leaks.
 	}
 	exit(g_exit);
 }
