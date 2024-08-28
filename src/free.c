4596c59c79a296dc92294b62724f40a1d93f96b5
Pablo Markaide <pablo.markaide@gmail.com>
2024-08-28 09:54:44 +0300

    SYNC

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 13:02:09 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/08/09 20:11:48 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/08/28 09:30:11 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,44 @@ void	free_array(char ***array)
 	}
 	free(*array);
 	*array = NULL;
+}
+
+void	free_string(char **str)
+{
+	if (str != NULL && *str != NULL)
+	{
+		free(*str);
+		*str = NULL;
+	}
+}
+
+void free_cmds(t_cmd *cmds)
+{
+	t_cmd	*tmp;
+
+	tmp = cmds;
+	while (cmds)
+	{
+		free_tokens(&tmp->cmd_arg);
+		free_tokens(&tmp->redir);
+		free(tmp);
+		tmp = cmds->next;
+	}
+}
+
+}
+
+void free_ins(t_macro *macro)
+{
+	//free_tokens
+	//free_cmds
+	//free_macro
+	free_tokens(macro->tokens);
+	
+}
+
+void free_all(t_macro *macro)
+{
+	//free_ins
+	//free_macro
 }
