ee5203c742b8262e9540137dd22536fda936b5a0
dbejar-s <149060545+dbejar-s@users.noreply.github.com>
2024-08-28 04:14:08 +0300

    Fixed exit code from exit_error

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: dbejar-s <dbejar-s@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:00:20 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/08/28 03:29:11 by dbejar-s         ###   ########.fr       */
+/*   Updated: 2024/08/28 04:12:07 by dbejar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,5 @@ void exit_error(char *file, char *msg, t_macro *macro, int exit_code)
 	ft_putstr_fd(msg, 2);
 	ft_putstr_fd("\n", 2);
 	macro->exit_code = exit_code;
-	//exit(exit_code);
+	exit(exit_code);
 }
