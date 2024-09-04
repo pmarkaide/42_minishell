cff30519ebd5d55ec215d382c4ab5c3b5301c9dc
dbejar-s <dbejar-s@student.hive.fi>
2024-09-04 18:12:27 +0300

    refactor: improve echo command handling

--- src/buin_echo.c
+++ src/buin_echo.c
@@ -6,24 +6,35 @@
 /*   By: dbejar-s <dbejar-s@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 09:04:32 by dbejar-s          #+#    #+#             */
-/*   Updated: 2024/08/28 02:09:53 by dbejar-s         ###   ########.fr       */
+/*   Updated: 2024/09/04 18:11:39 by dbejar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_echo2(char **args)
+static void	check_n(char **args, int *i, int *n_flag)
 {
-	int	i;
-	int	n_flag;
+	int	j;
 
-	i = 1;
-	n_flag = 0;
-	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0 && args[i][2] == '\0')
+	*i = 1;
+	*n_flag = 0;
+	while (args[*i] && args[*i][0] == '-' && args[*i][1] == 'n')
 	{
-		n_flag = 1;
-		i++;
+		j = 2;
+		while (args[*i][j] == 'n')
+			j++;
+		if (args[*i][j] == '\0')
+		{
+			*n_flag = 1;
+			(*i)++;
+		}
+		else
+			break ;
 	}
+}
+
+static void	print_echo(char **args, int i, int n_flag)
+{
 	while (args[i])
 	{
 		ft_putstr_fd(args[i], STDOUT_FILENO);
@@ -33,5 +44,14 @@ int	ft_echo2(char **args)
 	}
 	if (!n_flag)
 		ft_putchar_fd('\n', STDOUT_FILENO);
+}
+
+int	ft_echo2(char **args)
+{
+	int	i;
+	int	n_flag;
+
+	check_n(args, &i, &n_flag);
+	print_echo(args, i, n_flag);
 	return (0);
 }
