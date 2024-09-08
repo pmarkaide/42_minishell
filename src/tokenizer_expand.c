ef12b5dd992b3fae62cfad2333b505431f81e935
Pablo Markaide <pablo.markaide@gmail.com>
2024-09-08 12:51:51 +0300

    fix: remove leading spaces before argument expansion

--- src/tokenizer_expand.c
+++ src/tokenizer_expand.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 17:45:23 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/09/03 15:25:25 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/09/08 12:46:02 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ t_token	*expand_token(t_token *token, t_macro *macro)
 	expanded = get_expanded_ins(token->value, macro);
 	if (!expanded || *expanded == '\0')
 		return (NULL);
+	fix_redirections(expanded);
 	if (ft_strchr("\"", token->value[0]))
 	{
 		token->value = handle_literal_string(token, expanded);
